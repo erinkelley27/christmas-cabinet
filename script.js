dbde9ec28033223bd72090516a339dff47ccd23e
“Erin <erinkelley27@gmail.com>
2019-05-23 13:47:14 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let productInfo = [
+  {desc: 'Over two thousand years ago in the hills of Judea, there was a man named Zechariah who loved God faithfully and served as a priest in God’s temple. Zechariah and his wife Elizabeth wanted a child, but they were too old. One day, when Zechariah was in the temple burning incense, an angel came to him and told him that he would be blessed with a son and that he should name the baby John. Zechariah was surprised and frightened, and he didn’t believe what the angel said. Immediately, Zechariah was unable to speak. When the baby was finally born, Zechariah asked for a tablet so he could write the baby’s name --John-- just as the angel had instructed. At that moment, Zechariah was able to speak again. (Luke 1:5-23,57- 80)'},
+  {desc: ''}
+]
+
+let cards = querySelectorAll('.cards')
